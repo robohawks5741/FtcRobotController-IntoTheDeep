93e6bed3e2a0f7371708251a5c55dfb847d966a6
jacobalgraham <143833847+jacobalgraham@users.noreply.github.com>
2024-09-15 20:00:44 -0400

    Update and rename Utilities to Utilities.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Utilities.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.Drawing;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+public class Utilities {
+    private static final int MAX_MOTOR_CURRENT = 20;
+    public void checkPower(DcMotorEx motor) {
+        double power = motor.getPower();
+        if(motor.getCurrent(CurrentUnit.AMPS) > MAX_MOTOR_CURRENT) {
+            motor.setPower(power * MAX_MOTOR_CURRENT / motor.getCurrent(CurrentUnit.AMPS));
+        }
+    }
+}

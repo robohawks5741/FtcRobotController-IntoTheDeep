421c0db34bfb6256857adb3f60a9f9344a53b7d8
Jacob <143833847+jacobalgraham@users.noreply.github.com>
2025-02-04 18:00:59 -0500

    Work w/ actions/autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoLeft.java
@@ -2,9 +2,13 @@ package org.firstinspires.ftc.teamcode.auto;
 import static com.acmerobotics.roadrunner.ftc.Actions.runBlocking;
 
 import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Trajectory;
 import com.acmerobotics.roadrunner.Vector2d;
 
 
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.BotConstants;
@@ -22,14 +26,19 @@ public class AutoLeft extends AutoSuper {
         waitForStart();
         while (opModeIsActive()){
 
-            runBlocking(
+
+            Actions.runBlocking(new ParallelAction(
                     drive.actionBuilder(beginPose)
-                    .splineTo(new Vector2d(11.1396, 22.0307), Math.toRadians(-45.0))
-                    .stopAndAdd(new ServoAction(clawIntake, BotConstants.CLAW_CLOSED))
-                    .build()
-            );
+                            .splineTo(new Vector2d(11.1396, 22.0307), Math.toRadians(-45.0))
+                            .stopAndAdd(new ServoAction(clawIntake, BotConstants.CLAW_CLOSED))
+                            .build(),
+                    new ServoAction(clawIntake, 0)
+            ));
+
+
 
         }
+
     }
 
 }
